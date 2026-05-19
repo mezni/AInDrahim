47de7c0e059fb0af4245e6f845000f3c85840143
M.Mezni <mamezni@gmail.com>
2026-05-19 21:59:53 +0000

    React Fist app

---
+++ projects/MyFirstApp/App.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <Text>Hello World 🚀</Text>
+    </View>
+  );
+}
