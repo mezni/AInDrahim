ebedb768fdb2798adc5e1530c44b8cc358784486
M.Mezni <mamezni@gmail.com>
2026-08-22 09:23:30 -0400

    Chatbot: add streamlit

--- LangC/chatbot/app.py
+++ LangC/chatbot/app.py
@@ -6,4 +6,11 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
 
+LANGCHAIN_TRACING_V2="true"
+
+st.title("LangChain Chatbot")
+input_text = st.text_input("Ask a question")
